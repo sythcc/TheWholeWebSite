089b912dc70ea888bb7788ee8a8fc958a8c19077
lujun500 <1125037494@qq.com>
2015-01-25 23:00:42 +0800

    Signed-off-by: tryrtytyrtytryrtyrty

--- src/test/java/TestForDebug/TestForNG.java
+++ src/test/java/TestForDebug/TestForNG.java
@@ -13,4 +13,39 @@ public class TestForNG {
 	public void testCat2() {
 		Assert.assertEquals(true, true);
 	}
+	
+	@Test
+	public void testCat9() {
+		Assert.assertEquals(true, true);
+	}
+	
+	@Test
+	public void testCat3() {
+		Assert.assertEquals(false, true);
+	}
+	
+	@Test
+	public void testCat4() {
+		Assert.assertEquals(true, true);
+	}
+	
+	@Test
+	public void testCat5() {
+		Assert.assertEquals(true, true);
+	}
+	
+	@Test
+	public void testCat6() {
+		Assert.assertEquals(false, true);
+	}
+	
+	@Test
+	public void testCat7() {
+		Assert.assertEquals(true, true);
+	}
+	
+	@Test
+	public void testCat8() {
+		Assert.assertEquals(true, true);
+	}
 }
